4e64e61f97b141c4c17e01da704ceb930f0fe3e6
unknown <aktr.contact@gmail.com>
2022-11-08 09:46:05 +0100

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+
+Event.destroy_all
+User.destroy_all
+Attendance.destroy_all
+
+
+5.times do
+
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+
+    User.create!(first_name: first_name,
+        last_name: last_name,
+        description: Faker::TvShows::SouthPark.quote, 
+        email: "#{first_name.downcase}.#{last_name.downcase}@yopmail.com",
+        encrypted_password:"Lemotdepasse9/3")
+
+
+    end
+
+     
+
+    
